a4b924e39530b20ed16bae852aab59675a81c898
Riky <born.in.winter48@gmail.com>
2024-11-11 22:55:40 -0300

    "creando clase jackpoot777"

---
+++ Jackpot777.ts
@@ -0,0 +1,15 @@
+
+ export class Jackpot777 implements Juego {
+    
+    constructor(private numero: number, private puntos: number) {
+
+
+    }
+
+    public jugar(apuesta: number): string {
+
+ }
+    public resultado(): string {
+        
+ }
+}
