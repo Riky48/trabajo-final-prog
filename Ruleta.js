685cd9af6f97cf9fe1461ed7c9366b6e7f1a801d
alexBeltramella <beatsinka@gmail.com>
2024-11-15 17:10:28 -0300

    probe readlie sync para decirle al usuario que elija un numero y un color

--- Ruleta.js
+++ Ruleta.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Ruleta = void 0;
+var readlineSync = require("readline-sync");
 var Ruleta = /** @class */ (function () {
     function Ruleta(numeros, colores) {
         this.numeros = [];
-        this.colores = ["negro", "rojo"];
+        this.colores = ["rojo", "negro"];
         for (var i = 1; i <= 36; i++) {
             this.numeros.push(i);
         }
@@ -15,14 +16,46 @@ var Ruleta = /** @class */ (function () {
     Ruleta.prototype.getColor = function () {
         return this.colores;
     };
+    //METODO PARA GIRAR LA RULETA Y QUE ELIJA UN NUMERO Y COLOR ALETARIO 
     Ruleta.prototype.girarRuleta = function () {
         var numeroAleatorio = this.numeros[Math.floor(Math.random() * this.numeros.length)];
         var colorAleatorio = this.colores[Math.floor(Math.random() * this.colores.length)];
         return { numero: numeroAleatorio, color: colorAleatorio };
     };
+    //METODO PARA VERIFICAR SI LO QUE ELIJIO EL USUARIO CONISIDE CON EL RESULTADO
+    Ruleta.prototype.verificarApuesta = function (eleccion) {
+        var resultado = this.girarRuleta();
+        console.log("la ruleta cayo en el numero ".concat(resultado.numero, " y color ").concat(resultado.color, "."));
+        return eleccion.numero === resultado.numero && eleccion.color === resultado.color;
+    };
     return Ruleta;
 }());
 exports.Ruleta = Ruleta;
 var ruleta = new Ruleta();
-var resultado = ruleta.girarRuleta();
-console.log("la ruleta cayo en el numero ".concat(resultado.numero, " y color ").concat(resultado.color));
+//FUNCION PARA INICIAR LA APUESTA
+function iniciarApuesta() {
+    //preguntar al usuario que elija un numero
+    var numeroElegido = readlineSync.questionInt("Elige un numero entre 1 y 36: ");
+    //VALIDAR QUE EL NUMERO SE ENCUENTRA ENTRE 1 Y 36
+    if (numeroElegido < 1 || numeroElegido > 36) {
+        console.log("por favor elegir un numero entre 1 y 36");
+        return iniciarApuesta(); //vuelve a preguntar si no es valido
+    }
+    //PREGUNTAR AL USUARIO QUE ELIJA UN COLOR "negro" "rojo"
+    var elegirColor = readlineSync.question("Elegir color (negro, rojo): ").toLowerCase();
+    //VALIDAR COLOR ELEGIDO
+    if (elegirColor !== "negro" && elegirColor !== "rojo") {
+        console.log("por favor elige un color correcto: negro o rojo");
+        return; //vuelve a preguntar
+    }
+    //VERIFICAR APUESTA 
+    var eleccionUsuario = { numero: numeroElegido, color: elegirColor };
+    if (ruleta.verificarApuesta(eleccionUsuario)) {
+        console.log("Felicidades ganaste");
+    }
+    else {
+        console.log("lo siento sera la proxima");
+    }
+}
+//INICIAR APUESTA
+iniciarApuesta();
--- Ruleta.ts
+++ Ruleta.ts
@@ -1,10 +1,12 @@
+import * as readlineSync from "readline-sync";
+
 export class Ruleta {
     private numeros: number[];
     private colores: string[];
 
     constructor(numeros?:string[], colores?:string[]) {
         this.numeros = [];
-        this.colores = ["negro", "rojo"];
+        this.colores = ["rojo","negro"];
 
         for(let i = 1; i <= 36; i++) {
             this.numeros.push(i);
@@ -19,14 +21,58 @@ export class Ruleta {
         return this.colores;
     }
 
-    girarRuleta() {
+
+    //METODO PARA GIRAR LA RULETA Y QUE ELIJA UN NUMERO Y COLOR ALETARIO 
+    girarRuleta(): {numero:Number; color:String} {
         const numeroAleatorio = this.numeros[Math.floor(Math.random() * this.numeros.length)];
         const colorAleatorio = this.colores[Math.floor(Math.random() * this.colores.length)];
         return { numero:numeroAleatorio, color:colorAleatorio};
     }
+
+
+    //METODO PARA VERIFICAR SI LO QUE ELIJIO EL USUARIO CONISIDE CON EL RESULTADO
+    verificarApuesta(eleccion: {numero:number; color:string}):boolean {
+        const resultado = this.girarRuleta();
+        console.log(`la ruleta cayo en el numero ${resultado.numero} y color ${resultado.color}.`);
+        return eleccion.numero === resultado.numero && eleccion.color === resultado.color;
+        
+    }
+
 }
 
 const ruleta = new Ruleta();
 
-const resultado = ruleta.girarRuleta();
-console.log(`la ruleta cayo en el numero ${resultado.numero} y color ${resultado.color}`);
+//FUNCION PARA INICIAR LA APUESTA
+function iniciarApuesta() {
+    //preguntar al usuario que elija un numero
+    const numeroElegido = readlineSync.questionInt("Elige un numero entre 1 y 36: ");
+
+    //VALIDAR QUE EL NUMERO SE ENCUENTRA ENTRE 1 Y 36
+    if(numeroElegido < 1 || numeroElegido > 36) {
+        console.log("por favor elegir un numero entre 1 y 36" );
+        return iniciarApuesta();//vuelve a preguntar si no es valido
+    }
+
+    //PREGUNTAR AL USUARIO QUE ELIJA UN COLOR "negro" "rojo"
+    const elegirColor : string = readlineSync.question("Elegir color (negro, rojo): ").toLowerCase();
+
+    //VALIDAR COLOR ELEGIDO
+    if(elegirColor !== "negro" && elegirColor !== "rojo") {
+        console.log("por favor elige un color correcto: negro o rojo");
+        return; //vuelve a preguntar
+    }
+
+    //VERIFICAR APUESTA 
+    const eleccionUsuario = {numero:numeroElegido, color:elegirColor};
+    
+    if(ruleta.verificarApuesta(eleccionUsuario)) {
+        console.log("Felicidades ganaste");
+    }else {
+        console.log("lo siento sera la proxima");
+    }
+}
+
+
+//INICIAR APUESTA
+iniciarApuesta();
+
