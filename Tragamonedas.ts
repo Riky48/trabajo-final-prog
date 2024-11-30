25267535b07e05e7e9d02105e56ff53efb809dcb
Naty8 <168041378+Naty8@users.noreply.github.com>
2024-11-29 21:18:33 -0300

    Add files via upload

---
+++ Tragamonedas.ts
@@ -0,0 +1,17 @@
+import{Juego}from"./Juego";
+export abstract class Tragamonedas implements Juego{
+    protected tema: string;
+    protected apuestaMinima: number;
+    constructor(tema: string, apuestaMinima:number){
+        this.tema= tema;
+        this.apuestaMinima= apuestaMinima;
+    }
+    abstract jugar(apuesta:number):{mensaje: string; ganancia: number};
+    resultado():string{
+        return Resultados del juego${this.tema};
+    }
+    //Valida que la apuesta por el usuario sea mayor o igual a la apuesta minima
+    protected validarApuesta(apuesta:number):boolean{
+        return apuesta >= this.apuestaMinima;
+    }
+}
